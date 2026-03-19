51bb34f181f572eeb3b3348bd5d660f13c9a7dc1
cmendes <carlos.mendes@dcx.ufpb.br>
2026-03-19 19:48:00 -0300

    atividade 3 - Converter Json para objeto e imprimir alunos aprovados

---
+++ Frontend/atv3-jsonToList.js
@@ -0,0 +1,21 @@
+const todosAlunos = `[{ "nome": "Maria Joaquinha", "notas": [8,7.5,9], "curso": "Sistemas para Internet"}, { "nome": "João Ricardo", "notas": [8,8.5,5], "curso": "Direito"}, { "nome": "José Henrique", "notas": [4,10,7], "curso": "Administração"}, { "nome": "Pedro da Silva", "notas": [6,7.6,7.5], "curso": "Sistemas para Internet"}, { "nome": "Silvana Morais", "notas": [6,7.5,9.5], "curso": "Sistemas de Informação"}, { "nome": "Patricia Castro", "notas": [1,9,10], "curso": "Arquitetura"}, { "nome": "Joana Ribeiro", "notas": [8,9,9], "curso": "Contabilidade"}, { "nome": "Rafael Rocha", "notas": [4,4,9], "curso": "Sistemas para Internet"}, { "nome": "Gustavo Henrique", "notas": [8,7.5,5], "curso": "Sistemas para Internet"}]`;
+
+const listaAlunosConvertida = JSON.parse(todosAlunos);
+
+function exibeAlunosAprovados(alunos) {
+    console.log("-----------------------");
+
+    alunos.forEach(aluno => {
+        const soma = aluno.notas.reduce((acumulador, nota) => acumulador + nota, 0);
+        const media = soma / aluno.notas.length;
+
+        if (media >= 7) {
+            console.log(`Nome: ${aluno.nome}`);
+            console.log(`Média: ${media.toFixed(2)}`);
+            console.log(`Curso: ${aluno.curso}`);
+            console.log("-----------------------");
+        }
+    });
+}
+
+exibeAlunosAprovados(listaAlunosConvertida);
